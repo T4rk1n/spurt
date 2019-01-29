f5f1d452fd8c0566f4e70eeb0bb633274fc9a3f7
t4rk1n <t4rk@outlook.com>
2019-01-29 16:53:24 -0500

    :rotating_light: Test nested components parsing.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,8 +5,27 @@ parser = parser_factory()
 
 
 def test_parse_component():
-    tree = parser.parse('<html.Div ("children" "children 2") id="test">')
+    tree = parser.parse('''
+    <html.Div 
+        (
+            "children"
+            <html.Div 
+                ("children 2") 
+                id="nested-child"
+                style={backgroundColor: "purple"}
+            >
+        )
+        id="test"
+    >
+    ''')
     div = tree.children[0]
+
     assert isinstance(div, html.Div)
     assert div.id == 'test'
     assert len(div.children) == 2
+
+    nested = div.children[1]
+
+    assert nested.id == "nested-child"
+    assert 'backgroundColor' in nested.style
+
