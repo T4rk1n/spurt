53a4d409a9fe78c198bc0db602465f17a32f059e
Philippe Duval <philippe@plot.ly>
2019-01-30 13:34:05 -0500

    :rotating_light: Use dash_subprocess instead of threaded for tests.

---
+++ tests/apps/spurt_app.py
@@ -0,0 +1,18 @@
+from spurt import Spurt
+
+s = '''
+<html.Div
+    (
+        <html.H2 ("Spurt test")>
+        <dcc.Input id="input">
+        <html.Div
+            id="output"
+            # `@` for Input, the value is from callback argument
+            children=(@input.value) => @input.value
+        >
+    )
+>
+'''
+spurt_app = Spurt(__name__, s)
+app = spurt_app.app
+
--- tests/test_spurt.py
+++ tests/test_spurt.py
@@ -1,26 +1,9 @@
-import time
-
-from spurt import Spurt
-
 from pytest_dash.utils import wait_for_text_to_equal
 
 
-def test_spurt_app(dash_threaded, selenium):
-    s = '''
-    <html.Div
-        (
-            <html.H2 ("Spurt test")>
-            <dcc.Input id="input">
-            <html.Div
-                id="output"
-                # `@` for Input, the value is from callback argument
-                children=(@input.value) => @input.value
-            >
-        )
-    >
-    '''
-    app = Spurt(__name__, s)
-    dash_threaded(app.app)
-    _input = selenium.find_element_by_id('input')
+def test_spurt_app(dash_subprocess):
+    dash_subprocess('tests.apps.spurt_app')
+    driver = dash_subprocess.driver
+    _input = driver.find_element_by_id('input')
     _input.send_keys('Hello spurt')
-    wait_for_text_to_equal(selenium, '#output', 'Hello spurt')
+    wait_for_text_to_equal(driver, '#output', 'Hello spurt')
