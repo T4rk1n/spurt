a8494a5ae94c1746caaeec6ca9a4ae690b2de0e0
t4rk1n <t4rk@outlook.com>
2019-01-29 20:36:05 -0500

    :sparkles: Add spurt dash app wrapper.

--- spurt/__init__.py
+++ spurt/__init__.py
@@ -1,1 +1,3 @@
+from ._spurt import Spurt
+
 __version__ = '0.0.1'
---
+++ spurt/_spurt.py
@@ -0,0 +1,14 @@
+import dash
+
+from spurt.spurt_parser import parser_factory
+
+
+class Spurt:
+    def __init__(self,
+                 name,
+                 spurt,
+                 component_libraries=None,
+                 variables=None,):
+        self.app = dash.Dash(name)
+        self.parser = parser_factory(component_libraries, variables)
+        self.app.layout = self.parser.parse(spurt).children[0]
