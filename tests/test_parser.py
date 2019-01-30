c1c5a6478a96619e486599d95b54e25fdc0675dc
t4rk1n <t4rk@outlook.com>
2019-01-29 20:03:01 -0500

    :rotating_light: Test variables parsing.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,18 +1,21 @@
 import dash_html_components as html
 from spurt.spurt_parser import parser_factory
 
-parser = parser_factory()
-
 
 def test_parse_component():
+    parser = parser_factory(
+        variables={
+            'bg_color': 'purple'
+        }
+    )
     tree = parser.parse('''
-    <html.Div 
+    <html.Div
         (
             "children"
-            <html.Div 
+            <html.Div
                 ("children 2") 
                 id="nested-child"
-                style={backgroundColor: "purple"}
+                style={backgroundColor: $bg_color}
             >
         )
         id="test"
@@ -27,5 +30,4 @@ def test_parse_component():
     nested = div.children[1]
 
     assert nested.id == "nested-child"
-    assert 'backgroundColor' in nested.style
-
+    assert nested.style['backgroundColor'] == 'purple'
