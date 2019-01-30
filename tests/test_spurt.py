f74afd5526e669ddb01dcf73cc854aecaf30659b
t4rk1n <t4rk@outlook.com>
2019-01-29 21:19:52 -0500

    :rotating_light: Improve test_spurt to include callbacks.

--- tests/test_spurt.py
+++ tests/test_spurt.py
@@ -2,19 +2,23 @@ import time
 
 from spurt import Spurt
 
+from pytest_dash.utils import wait_for_text_to_equal
 
-def test_spurt_app(dash_threaded):
+
+def test_spurt_app(dash_threaded, selenium):
     s = '''
     <html.Div
         (
             <dcc.Input id="input">
             <html.Div
                 id="output"
-                children=(input.value) => input.value
+                children=(@input.value) => @input.value
             >
         )
     >
     '''
     app = Spurt(__name__, s)
     dash_threaded(app.app)
-    time.sleep(2)
+    _input = selenium.find_element_by_id('input')
+    _input.send_keys('Hello spurt')
+    wait_for_text_to_equal(selenium, '#output', 'Hello spurt')
